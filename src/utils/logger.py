0ae001da1fe7cb6dd553a2e985c48cd369cb875e
forumvmatr67-oss <forumvmatr67@gmail.com>
2026-05-11 18:50:07 +0300

    Create logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,16 @@
+"""Настройка логирования."""
+
+import logging
+import sys
+
+def setup_logger(name: str = "minecraft_bot", level=logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
